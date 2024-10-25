cba287d9f01c4af64c1f6e8abda73f19c9814b24
jsharp <joshua.sharp@communitybrands.com>
2024-10-25 14:35:50 -0500

    Days 9 and 10 complete

---
+++ Day 10 - Adapter Array/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdapterArray;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var inputOrdered = File.ReadAllLines("input.txt").Select(i => int.Parse(i)).Order().ToList();
+        inputOrdered.Insert(0, 0);
+        inputOrdered.Add(inputOrdered.Max() + 3);
+        Part1(inputOrdered);
+        Part2(inputOrdered);
+    }
+
+    static void Part1(List<int> input){
+        var count3 = 0;
+        var count1 = 0;
+
+        for(int i = 0; i < input.Count - 1; i++){
+            var dif = input[i + 1] - input[i];
+            if (dif == 1) count1++;
+            if (dif == 3) count3++;
+        }
+
+        Console.WriteLine($"Part 1: {count1 * count3}");
+    }
+
+    static void Part2(List<int> input){
+        Dictionary<int, long> pathCount = new Dictionary<int, long>();
+        foreach(var num in input) pathCount.Add(num, 0);
+        pathCount[input.First()] = 1;
+        
+        for(int i = 0; i < input.Count - 1; i++){
+            for(int n = i + 1; n < i + 4 && n < input.Count; n++){
+                if(input[n] - input[i] <= 3){
+                    pathCount[input[n]] += pathCount[input[i]];
+                }
+                else{
+                    break;
+                }
+            }
+        }
+
+        Console.WriteLine($"Distinct Arragements: {pathCount[input.Last()]}");
+    }
+}
---
+++ Day 9 - Encoding Error/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace EncodingError;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var input = File.ReadAllLines("input.txt").Select(l => long.Parse(l)).ToArray();
+        var firstBad = FindFirstBadRecord(input, 25);
+        Console.WriteLine($"First bad number: {firstBad}");
+
+        var weakness = FindWeakness(input, firstBad);
+        Console.WriteLine($"Weakness is: {weakness}");  
+    }
+
+    static long FindWeakness(long[] input, long badNumber){
+        var end = Array.IndexOf(input, badNumber);
+        long min = long.MaxValue;
+        long max = long.MinValue;
+        for(int i = 0; i < end; i++){
+            long total = 0;
+            for(int n = i; n < end; n++){
+                var num = input[n];
+                total += num;
+                if(num < min){
+                    min = num;
+                }
+                if(num > max){
+                    max = num;
+                }
+
+                if(total == badNumber){
+                    return max + min;
+                }
+                if(total > badNumber){
+                    min = long.MaxValue;
+                    max = long.MinValue;
+                    break;
+                }
+            }
+        }
+        return long.MinValue;
+    }
+
+    static long FindFirstBadRecord(long[] input, long bufferSize){
+        for(long i = bufferSize; i < input.Length; i++){
+            bool found = false;
+            for(long n = i - bufferSize; n < i + bufferSize + 1; n++){
+                for (long t = i - bufferSize; t < i + bufferSize + 1; t++)
+                {
+                    if (input[t] == input[n]) continue;
+                    if(input[t] + input[n] == input[i]){
+                        found = true;
+                        break;
+                    }
+                    if (found) break;
+                }
+            }
+            if (!found) return input[i];
+        }
+        return long.MaxValue;
+    }
+}
