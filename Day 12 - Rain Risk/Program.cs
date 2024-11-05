08ff4b10315d73a7c2494011585770d792a2e460
jsharp <joshua.sharp@communitybrands.com>
2024-11-05 13:40:18 -0600

    Day 12 and Day 13 finally done

---
+++ Day 12 - Rain Risk/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace RainRisk;
+
+class Program
+{
+    public static Dictionary<char, Dictionary<int, char>> turns = new Dictionary<char, Dictionary<int, char>>(){
+        {'N', new Dictionary<int, char>(){
+            {90, 'E'},
+            {180, 'S'},
+            {270, 'W'}
+        }},
+        {'E', new Dictionary<int, char>(){
+            {90, 'S'},
+            {180, 'W'},
+            {270, 'N'}
+        }},
+        {'S', new Dictionary<int, char>(){
+            {90, 'W'},
+            {180, 'N'},
+            {270, 'E'}
+        }},
+        {'W', new Dictionary<int, char>(){
+            {90, 'N'},
+            {180, 'E'},
+            {270, 'S'}
+        }},
+    };
+
+    static void Main(string[] args)
+    {
+        var input = File.ReadAllLines("input.txt").Select(l => new Tuple<char, int>(l[0], int.Parse(l.Substring(1)))).ToArray();
+        var dist = FindDistanceAfterRun(input);
+        Console.WriteLine("Part 1 Distance: {0}", dist);
+
+        dist = FindDistanceAfterRunWithWayPoint(input);
+        Console.WriteLine("Part 2 Distance: {0}", dist);
+    }
+
+    static int FindDistanceAfterRun(Tuple<char, int>[] input){
+        var facing = 'E';
+        var north = 0;
+        var east = 0;
+
+        foreach (var ins in input){
+            switch (ins.Item1 == 'F' ? facing : ins.Item1) {
+                case 'N':
+                    north += ins.Item2;
+                    break;
+                case 'S':
+                    north -= ins.Item2;
+                    break; 
+                case 'E':
+                     east += ins.Item2;
+                    break;
+                case 'W':
+                     east -= ins.Item2;
+                    break;
+                case 'R':
+                    facing = turns[facing][ins.Item2];
+                    break;
+                case 'L':
+                    facing = turns[facing][360 - ins.Item2];
+                    break;
+            }
+        }
+        return Math.Abs(north) + Math.Abs(east);
+    }
+
+    static int FindDistanceAfterRunWithWayPoint(Tuple<char, int>[] input){
+        var waypointnorth = 1;
+        var waypointeast = 10;
+        var shipnorth = 0;
+        var shipeast = 0;
+
+        var tempwaypointeast = 0;
+
+        foreach (var ins in input){
+            switch (ins.Item1) {
+                case 'N':
+                    waypointnorth += ins.Item2;
+                    break;
+                case 'S':
+                    waypointnorth -= ins.Item2;
+                    break; 
+                case 'E':
+                     waypointeast += ins.Item2;
+                    break;
+                case 'W':
+                     waypointeast -= ins.Item2;
+                    break;
+                case 'F':
+                    shipnorth += waypointnorth * ins.Item2;
+                    shipeast += waypointeast * ins.Item2;
+                    break;
+                case 'R':
+                    switch(ins.Item2){
+                        case 90:
+                            tempwaypointeast = waypointeast;
+                            waypointeast = waypointnorth;
+                            waypointnorth = tempwaypointeast * -1;
+                            break;
+                        case 180:
+                            tempwaypointeast = waypointeast;
+                            waypointnorth = waypointnorth * -1;
+                            waypointeast = tempwaypointeast * -1;
+                            break;       
+                        case 270:
+                            tempwaypointeast = waypointeast;
+                            waypointeast = waypointnorth * -1;
+                            waypointnorth = tempwaypointeast;
+                            break;                     
+                    }
+                    break;
+                case 'L':
+                    switch(ins.Item2){
+                        case 90:
+                            tempwaypointeast = waypointeast;
+                            waypointeast = waypointnorth * -1;
+                            waypointnorth = tempwaypointeast;
+                            break;
+                        case 180:
+                            tempwaypointeast = waypointeast;
+                            waypointnorth = waypointnorth * -1;
+                            waypointeast = tempwaypointeast * -1;
+                            break;       
+                        case 270:
+                            tempwaypointeast = waypointeast;
+                            waypointeast = waypointnorth;
+                            waypointnorth = tempwaypointeast * -1;
+                            break;                     
+                    }
+                    break;
+            }
+        }
+        return Math.Abs(shipnorth) + Math.Abs(shipeast);
+    }
+}
---
+++ Day 13 - Shuttle Search/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+
+namespace ShuttleSearch;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var input = File.ReadAllLines("input.txt");
+        var target = int.Parse(input[0]);
+        string[] busses = input[1].Split(',');
+
+        Console.WriteLine("Part 1: {0}", Part1(target, busses));
+        Console.WriteLine("Part 2: {0}", Part2(busses));
+    }
+
+    static long Part1(long target, string[] busses)
+    {
+        var track = new List<Tuple<int, long>>();
+        foreach (var bus in busses)
+        {
+            if (int.TryParse(bus, out var id))
+            {
+                track.Add(new Tuple<int, long>(id, nextOccurence(target, id) - target));
+            }
+        }
+        var closest = track.OrderBy(t => t.Item2).First();
+        return closest.Item1 * closest.Item2;
+    }
+
+    public static long nextOccurence(long target, int id) => (target / id * id) + id;
+
+    static long Part2(string[] input)
+    {
+        List<long> values = new List<long>();
+        List<long> remainders = new List<long>();
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (int.TryParse(input[i], out int id)) {
+                values.Add(id);
+                remainders.Add(id - i);
+            }
+        }
+
+        return ChineseRemainder(values, remainders);
+    }
+
+    //Slower solution
+    // static long Part2(string[] busses)
+    // {
+    //     var step = busses.Select(t => int.Parse(t)).First();
+    //     long timestamp = 100000000000000;
+    //     for(int i = 1; i < busses.Length; i++){
+    //         if(int.TryParse(busses[i], out int id)){
+    //             while(nextOccurence(timestamp, id) - timestamp != i){
+    //                 timestamp += step;
+    //             }
+    //             step *= id;
+    //         }
+    //     }
+    //     return timestamp;
+    // }
+
+    static long ChineseRemainder(List<long> values, List<long> remainders){
+        var moduliProduct = values.Aggregate(1L, (a, b) => a * b);
+
+        var vals = new List<long>();
+
+        for (int i = 0; i < values.Count; i++)
+        {
+            var coefficeint = moduliProduct / values[i];
+            var x = coefficeint % values[i];
+            long count = modInverse(x, values[i]);
+            vals.Add(remainders[i] * coefficeint * count);
+        }
+
+        return vals.Aggregate(1L, (a, b) => a + b) % moduliProduct - 1;
+    }
+
+    static long modInverse(long a, long n)
+    {
+        long i = n;
+        long v = 0L,  d = 1L;
+        while (a > 0)
+        {
+            long t = i / a, x = a;
+            a = i % x;
+            i = x;
+            x = d;
+            d = v - t * x;
+            v = x;
+        }
+        v %= n;
+        if (v < 0) v = (v + n) % n;
+        return v;
+    }
+
+}
